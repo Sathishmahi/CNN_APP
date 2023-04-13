79233e46a2f563f4e4a90eaafe0366c12ec8ac08
Sathishmahi <sathishmahi433@gmaoil.com>
2023-04-13 17:51:25 +0530

    modify stage 001 data ingestion

--- src/Classifier/pipeline/stage_001_data_ingestion.py
+++ src/Classifier/pipeline/stage_001_data_ingestion.py
@@ -1,16 +1,18 @@
 from src.Classifier.components import DataIngestion
 from Classifier import logger
 
+
 def main():
-    logger.info(f'>>>> start data ingestion ')
-    data_inge=DataIngestion()
+    logger.info(f">>>> start data ingestion ")
+    data_inge = DataIngestion()
     data_inge.download_data()
     data_inge.extract_data()
-    logger.info(f'x=========  data ingestion finish  =========X')
+    logger.info(f"x=========  data ingestion finish  =========X")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     try:
         main()
     except Exception as e:
         logger.exception(msg=e)
-        raise e 
+        raise e
