740febbf2f9b6da8c9737c8e86fa7a117b1cd104
Sathishmahi <sathishmahi433@gmaoil.com>
2023-04-13 17:52:34 +0530

    update init classifier

--- src/Classifier/__init__.py
+++ src/Classifier/__init__.py
@@ -1,21 +1,18 @@
-import logging 
+import logging
 import os
 import sys
 
 
-log_dir="logs"
-os.makedirs(log_dir,exist_ok=True)
-log_file_path=os.path.join(
-    log_dir,
-    "running_logs.log"
-)
-format="[ %(asctime)s    %(levelname)   s%(module)s ]  [ %(message)s ]"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+log_file_path = os.path.join(log_dir, "running_logs.log")
+format = "[ %(asctime)s    %(levelname)   s%(module)s ]  [ %(message)s ]"
 logging.basicConfig(
-level=logging.INFO,
-format=format,
-handlers=[
-    logging.FileHandler(log_file_path),
-    # logging.StreamHandler(sys.stdout)
-]
+    level=logging.INFO,
+    format=format,
+    handlers=[
+        logging.FileHandler(log_file_path),
+        # logging.StreamHandler(sys.stdout)
+    ],
 )
-logger=logging.getLogger("ClassifierLogger")
+logger = logging.getLogger("ClassifierLogger")
