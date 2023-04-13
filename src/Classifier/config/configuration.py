be4ecc6684277889b378380ed07a6647d95ec1dc
Sathishmahi <sathishmahi433@gmaoil.com>
2023-04-13 16:29:09 +0530

    configuration package updated

---
+++ src/Classifier/config/configuration.py
@@ -0,0 +1,26 @@
+from Classifier.constant import *
+from Classifier.entity.config_entity import DataIngestionConfig
+from Classifier.utils import read_yaml
+
+
+
+class Configuration:
+    def __init__(self,config_file_path=CONFIG_FILE_PATH,
+    params_file_path=PARAMS_FILE_PATH):
+        self.config=read_yaml(path_to_yaml=config_file_path)
+        self.params=read_yaml(path_to_yaml=params_file_path)
+
+        self.data_ingestion_config=self.get_data_ingestion_config()
+
+
+    def get_data_ingestion_config(self)->DataIngestionConfig:
+
+        data_injection_config_attr=self.config.get("data_ingestion")
+        data_injection_config=DataIngestionConfig(
+        root_dir=data_injection_config_attr.get("root_dir"),
+        source_URL=data_injection_config_attr.get("source_URL"),
+        local_data_file=data_injection_config_attr.get("local_data_file"),
+        unzip_dir=data_injection_config_attr.get("unzip_dir")
+        )
+
+        return data_injection_config
---
+++ src/Classifier/entity/config_entity.py
@@ -0,0 +1,10 @@
+from collections import namedtuple
+
+
+DataIngestionConfig=namedtuple("DataIngestionConfig", [
+    "root_dir",
+    "source_URL",
+    "local_data_file",
+    "unzip_dir"
+
+])
