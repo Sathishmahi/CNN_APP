2038c050d61ed416ca18955633c44d191c0b4851
Sathishmahi <sathishmahi433@gmail.com>
2023-04-13 10:08:31 +0530

    test added

--- template.py
+++ template.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO,filename='loginfo.log',format='[%(asctime
 package_name='Classifier'
 
 list_of_files=[
-    '.github/workflows/.gitkeep',
+'.github/workflows/.gitkeep',
 f'src/{package_name}/__init__.py',
 f'src/{package_name}/components/__init__.py',
 f'src/{package_name}/config/__init__.py',
@@ -16,6 +16,9 @@ f'src/{package_name}/constant/__init__.py',
 f'src/{package_name}/pipeline/__init__.py',
 f'src/{package_name}/entity/__init__.py',
 f'src/{package_name}/__init__.py',
+"tests/__init__.py",
+"tests/unit/__init__.py",
+"tests/integration/__init__.py",
 'config/config.yaml',
 'params.yaml',
 'dvc.yaml',
