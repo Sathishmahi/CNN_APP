06e2c2d80f72f4501598d0afebc157d7468fb058
Sathishmahi <sathishmahi433@gmail.com>
2023-04-16 13:28:12 +0530

    update stage 02

--- src/Classifier/pipeline/stage_02_prepare_base_model.py
+++ src/Classifier/pipeline/stage_02_prepare_base_model.py
@@ -5,7 +5,7 @@ def main():
     logger.info(f">>>> start data ingestion ")
     preapare_base_model = PreapareBaseModel()
     preapare_base_model.update_base_model()
-    logger.info(f"x=========  prepare base model finish finish  =========X")
+    logger.info(f"x========  prepare base model finish finish  =========X")
 
 
 if __name__ == "__main__":
